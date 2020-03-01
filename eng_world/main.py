9adecf603b5aaf63c41feb6fbbd04140b93ce647
studentwithpc <kossiopia91@gmail.com>
2020-03-01 23:55:32 +0300

    version 1.0, jnly test mode

--- eng_world/main.py
+++ eng_world/main.py
@@ -11,33 +11,34 @@ def count_knowledge(frame, key="done"):
     kn=np.count_nonzero(frame.status==key)
     return kn
 
-def learn_mode(frame):
+def first_test_mode(frame):
 
     num=rm.randint(0,len(frame)-1)
-    if frame.status[num]=="done":
-        learn_mode(frame)
-    else:
-        print("New word :\n\n")
-        print(frame.eng_word[num])#, " - ", frame.rus_word[num])
-        ans = input("\n\nDo you know this word? y/n\n")
+    while frame.status[num]=="done" or frame.status[num]=="learning":
+         print("repeat", num)
+         num=rm.randint(0,len(frame)-1)
+
+    print("New word :\n\n")
+    print(frame.eng_word[num])  # , " - ", frame.rus_word[num])
+    ans = input("\n\nDo you know this word? y/n\n")
+    if ans == "y":
+        print("\n\n - ", frame.rus_word[num])
+        ans = input("\n\nAre you sure?\n")
         if ans == "y":
-            print("\n\n - ", frame.rus_word[num])
-            ans=input("\n\nAre you sure?\n")
-            if ans=="y":
-                frame.status[num] = "done"
-            else:
-                frame.status[num] = "learning"
+            frame.status[num] = "done"
         else:
-            print("\n\n - ",frame.eng_word[num]," - ", frame.rus_word[num],"\n\n")
             frame.status[num] = "learning"
+    else:
+        print("\n\n - ", frame.eng_word[num], " - ", frame.rus_word[num], "\n\n")
+        frame.status[num] = "learning"
 
 
 def start(path):
     frame = read_file(path)
 
-    learn_mode(frame)
+    first_test_mode(frame)
     frame.to_csv(path, index=False)
-    return (count_knowledge(frame), count_knowledge(frame,"learning"))
+    return (count_knowledge(frame), count_knowledge(frame,"learning"),len(frame)-1)
 
 if __name__== "__main__":
     path="./words/res_merge.csv"
@@ -53,7 +54,7 @@ if __name__== "__main__":
         if ans=="n":
             break
         elif ans=="y":
-            kn,pr=start(path)
+            kn,pr,all=start(path)
             count+=1
             print(" In this session you learn {} words".format(count))
 
@@ -63,6 +64,7 @@ if __name__== "__main__":
 
     print("Your status:\n You know {} words.\n In process {} words".format(kn,pr))
     print("You know {} % printing words".format(round(kn/(kn+pr)*100)))
+    print("Here {} words. i see {} words".format(all, kn+pr))
     if count >5:
         print("Good work! See you leter!")
     else:
